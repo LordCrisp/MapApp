a7f2a07082504ad725971e34fa215718ad194e5f
LordCrisp <lord_crisp@hotmail.com>
2018-04-05 13:13:53 +0200

    First Commit :unicorn:

---
+++ assets/js/loader.js
@@ -0,0 +1,8 @@
+// Create a tag (div?) with a class .loader__main with the loading screen styled.
+
+<!-- Loader Script -->
+<script type="text/javascript">
+    jQuery(document).ready(function() {
+        jQuery(".loader__main").remove();
+    });
+</script>
