8de0a28c323d1f763288d025120ea148a535993e
LordCrisp <lord_crisp@hotmail.com>
2018-04-06 10:43:36 +0200

    Service Worker Play

--- app/service-worker.js
+++ app/service-worker.js
@@ -9,9 +9,9 @@ var filesToCache = [
 ];
 
 //Install Service Worker and Cache App Content
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function(e) {
     console.log('Service Worker Installing.');
-    event.waitUntil(
+    e.waitUntil(
         caches.open(cacheName).then(function (cache) {
             console.log('Service Worker Caching App Shell');
             return cache.addAll(filesToCache);
@@ -20,16 +20,16 @@ self.addEventListener('install', function(event) {
 });
 
 //Service Worker Activation
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function() {
     console.log('Service Worker Activating.');
 });
 
 //Service Worker Fetch for offline
-self.addEventListener('fetch', function (event) {
-    console.log('Service Worker Fetching App Data', event.request.url);
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+self.addEventListener('fetch', function(e) {
+    console.log('Service Worker Fetching App Data', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
         })
     )
 })
