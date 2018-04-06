acae81c24aa8c938826fbca4c9e69e30eab4adbb
LordCrisp <lord_crisp@hotmail.com>
2018-04-06 10:32:53 +0200

    Service Worker Play

--- app/service-worker.js
+++ app/service-worker.js
@@ -6,7 +6,7 @@ var filesToCache = [
     "/index.html",
     "/assets/css/master.css",
     "/assets/js/loader.js"
-]
+];
 
 //Install Service Worker and Cache App Content
 self.addEventListener('install', function(event) {
