8933c9ddc892c31844fb2741c56b4b3c94b9a056
LordCrisp <lord_crisp@hotmail.com>
2018-04-06 08:48:58 +0200

    Service Worker Play

--- app/service_worker.js
+++ app/service_worker.js
@@ -1,5 +1,14 @@
 self.addEventListener('fetch', function (evt) {
-
+    if (event.request.url.indexOf('https://maps.googleapi.com/js') == 0) {
+        event.respondWith(
+            // Handle Maps API requests in a generic fashion,
+            // by returning a Promise that resolves to a Response.
+        );
+    } else {
+        event.respondWith(
+            // Some default handling.
+        );
+    }
 } );
 
 console.log("Fisk");
