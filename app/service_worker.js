01626765b156c380c98313d03478127737d7c9e1
LordCrisp <lord_crisp@hotmail.com>
2018-04-06 08:57:53 +0200

    Service Worker Play

--- app/service_worker.js
+++ app/service_worker.js
@@ -1,3 +1,14 @@
+//Service Worker Installer
+self.addEventListener('install', function(event) {
+    console.log('Service Worker installing.');
+});
+
+//Service Worker Activation
+self.addEventListener('activate', function(event) {
+    console.log('Service Worker activating.');
+});
+
+//Service Worker Fetch for offline
 self.addEventListener('fetch', function (evt) {
     if (event.request.url.indexOf('https://maps.googleapi.com/js') == 0) {
         event.respondWith(
