ea561a1ef7e0a6012176c793bf965e4c1957f971
LordCrisp <lord_crisp@hotmail.com>
2018-04-06 09:53:41 +0200

    Service Worker Play

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,15 +1,38 @@
 console.log("Hamster");
+
+var cacheName = 'MapAppCache';
+var filesToCache = [
+
+]
+
+//Install Service Worker and Cache App Content
+self.addEventListener('install', function(event) {
+    console.log('Service Worker installing.');
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('Service Worker Caching App Shell');
+            return cache.addAll(filesToCache);
+        })
+    )
+});
+
+//
+self.addEventListener('activate', function(event) {
+    console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log('Service Worker Fetching App Data', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        }
+        )
+    )
+})
+
 //Service Worker Fetch for offline
-self.addEventListener('fetch', function (evt) {
-    if (event.request.url.indexOf('https://maps.googleapi.com/js') == 0) {
-        event.respondWith(
-            // Handle Maps API requests in a generic fashion,
-            // by returning a Promise that resolves to a Response.
-        );
-    } else {
-        event.respondWith(
-            // Some default handling.
-        );
-    }
+self.addEventListener('fetch', function (event) {
+
 } );
 
