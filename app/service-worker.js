bc4519ab6bdade2ea224f42624e3e375e62705eb
LordCrisp <lord_crisp@hotmail.com>
2018-04-06 10:27:39 +0200

    Service Worker Play

--- app/service-worker.js
+++ app/service-worker.js
@@ -5,8 +5,7 @@ var filesToCache = [
     "/",
     "/index.html",
     "/assets/css/master.css",
-    "/assets/js/loader.js",
-    "/assets/js/maps.js"
+    "/assets/js/loader.js"
 ]
 
 //Install Service Worker and Cache App Content
